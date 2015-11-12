4794441fb8aa9a901fcde31eb25e48656267a2b0
shinijoker <zeh_timm@hotmail.com>
2015-11-12 21:55:54 -0200

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
 #include<stdio.h>
 
 int main(int argc, char ** argv){
-int tam
-double dados[v]
+int tam, i;
+double dados[v], dtotal;
 	char response;
 	while( 'q' != (response = getchar())){
 	print("Digite quantos dados são:");
 	scanf("%d", &tam);
-	
+	for(i=0;i<tam;i++){
+		scanf("%ls", &dados[v]);
+		dtotal += v;
+		
+	}
 	}
 	return 0;
 }
