a55a49bd1515bb1e7ff37d7d5ad59981014ec725
shinijoker <zeh_timm@hotmail.com>
2015-11-12 21:34:01 -0200

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main(int argc, char argv **){
 	
 	char response;
 	while( 'q' != response = getchar()){
-A DLÇ NUNCA EH PLENA, ENVENENA MATA A ALMA E ABRE O OCO
+A DLÇ NUNCA EH PLENA, MATA A ALMA E ABRE O OCO
 
 
 
