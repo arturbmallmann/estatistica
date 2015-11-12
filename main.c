87ee64442bdbde0cd2a5b9f7333cf2d35f2ee23c
shinijoker <zeh_timm@hotmail.com>
2015-11-12 21:46:34 -0200

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 
 int main(int argc, char ** argv){
-	
+int tam
+double dados[v]
 	char response;
 	while( 'q' != (response = getchar())){
 	print("Digite quantos dados são:");
+	scanf("%d", &tam);
+	
 	}
 	return 0;
 }
