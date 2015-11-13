5ef9708c64103e51c7c55de4414a15ed0260d918
artur <artur@biodevel.com>
2015-11-12 23:00:58 -0200

    agoravai

--- main.c
+++ main.c
@@ -9,20 +9,20 @@ int main(int argc, char ** argv){
 			double dados[tam], dtotal=0,dpadrao=0;
 			
 				for(i=0;i<tam;i++){
-					scanf("%ls", &dados[i]);
+					scanf("%lf", &dados[i]);
 					dtotal += dados[i];
 				}
 
-				media = dtotal/tam;
-				printf("Media: %.2ls\n", media);
-				
+				media = dtotal/(double)tam;
+				printf("Media: %.2lf\n", media);
+							
 				for(i=0;i<tam;i++){
 					int valor = dados[i]-media;
 					dpadrao += valor*valor;
 				}
-				printf("Desvio padrao: %.2ls\n", sqrt(dpadrao/tam));
+				printf("Desvio padrao: %.2lf\n", sqrt(dpadrao/tam));
 				cv = (sqrt(dpadrao/tam)/media)*100;
-				printf("Coeficiente de variaçao: %.2ls", cv);
+				printf("Coeficiente de variaçao: %.2lf %%\n", cv);
 			
 	return 0;
 }
